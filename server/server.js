31dd80f1981a12e523cfff2b4d0a676cb51d6643
Dan Lyons <danielclyons@yahoo.com>
2021-08-11 13:19:36 -0400

    working on deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
@@ -7,6 +8,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // create a new Apollo server and pass in our schema data
@@ -21,7 +23,7 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+console.log("process env is:", process.env.NODE_ENV)
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
