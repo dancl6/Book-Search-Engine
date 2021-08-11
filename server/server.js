b2e49db4c502433f803308c754ef1500bc70052f
Dan Lyons <danielclyons@yahoo.com>
2021-08-10 23:07:02 -0400

    working on deployment

--- server/server.js
+++ server/server.js
@@ -25,11 +25,16 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  
+} else {
+  app.get("/", (req,res) => {
+    res.send("Api running")
+  })
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 // app.use(routes); //comment this out
 
