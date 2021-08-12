7789111a03e4450552b1a5fa73e9c9df1053ba2c
Dan Lyons <danielclyons@yahoo.com>
2021-08-12 16:39:16 -0400

    deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.REACT_APP_MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
